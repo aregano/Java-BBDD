3bbd4284a65ba7ebfc94e3d915fe0c9361fb9cd1
Alvaro <alvaro.regano@gmail.com>
2018-11-26 18:04:27 +0100

    Java 9

--- AndresBank_ric/src/test/java/com/andresbank/dao/ClienteDAOTest.java
+++ AndresBank_ric/src/test/java/com/andresbank/dao/ClienteDAOTest.java
@@ -21,6 +21,7 @@ public class ClienteDAOTest extends TestAndres{
 
 	@Test
 	public void getUserByDNIAndPassVoid(){
+
 		String dni = "";
 		String pass ="";
 		
@@ -39,4 +40,69 @@ public class ClienteDAOTest extends TestAndres{
 
 	}
 
+	@Test
+	public void getUserByDNIAndPassDNI(){
+
+		String dni = "12345678P";
+		String pass ="";
+		
+		Cliente clienteEncontrado;
+		try {
+			clienteEncontrado = ClienteDAO.getInstance().getUserByDNIAndPass(dni, pass);
+			assertNull(clienteEncontrado);
+			
+			
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+		
+
+
+	}
+	
+	@Test
+	public void getUserByDNIAndPassDNIPass(){
+
+		String dni = "12345678R";
+		String pass ="0000";
+		
+		Cliente clienteEncontrado;
+		try {
+			clienteEncontrado = ClienteDAO.getInstance().getUserByDNIAndPass(dni, pass);
+			assertTrue(clienteEncontrado.getDni().equals(dni) && clienteEncontrado.getPin().equals(pass));
+			
+			
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+		
+
+
+	}
+	
+	@Test
+	public void getUserByDNIAndPassPass(){
+
+		String dni = "12345678P";
+		String pass ="xxx";
+		
+		Cliente clienteEncontrado;
+		try {
+			clienteEncontrado = ClienteDAO.getInstance().getUserByDNIAndPass(dni, pass);
+			assertNull(clienteEncontrado);
+			
+			
+
+		} catch (Exception e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+		
+
+
+	}
 }
